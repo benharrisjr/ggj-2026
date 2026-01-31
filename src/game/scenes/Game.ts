eb6caed084d5856db26cabf33ccf9c13f60f0f75
Benjamin Harris Jr <benjaminharrisjr@gmail.com>
2026-01-30 22:11:11 -0500

    masking

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -5,9 +5,11 @@ export class Game extends Scene {
     background: Phaser.GameObjects.Image;
     player: Phaser.Physics.Arcade.Image; // Changed to Arcade.Image for physics
     cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    maskGraphics: Phaser.GameObjects.Graphics;
 
     constructor() {
         super('Game');
+        
     }
 
     create() {
@@ -15,15 +17,30 @@ export class Game extends Scene {
         this.camera.setZoom(1.0);
         this.camera.setBackgroundColor(0x00ff00);
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+        // this.background = this.add.image(512, 384, 'background');
+        // this.background.setAlpha(0.5);
 
         // Enable physics for the player
-        this.player = this.physics.add.image(512, 384, 'player');
+        this.player = this.physics.add.image(0, 0, 'player');
         this.player.setCollideWorldBounds(true); // Prevent the player from leaving the screen
 
         // Create cursor keys for input
         this.cursors = this.input.keyboard.createCursorKeys();
+
+        const overlay = this.add.graphics();
+
+        overlay.fillStyle(0x000000, 0.8).fillRect(0, 0, 800, 600);
+
+        this.maskGraphics = this.make.graphics();
+
+        this.maskGraphics.fillStyle(0xffffff);
+        this.maskGraphics.fillRect(this.player.x, this.player.y, 64, 64);
+
+        const mask = new Phaser.Display.Masks.BitmapMask(this, this.maskGraphics);
+
+        mask.invertAlpha = true;
+
+        overlay.setMask(mask);
     }
 
     update() {
@@ -46,5 +63,6 @@ export class Game extends Scene {
 
         // Normalize diagonal movement
         this.player.body.velocity.normalize().scale(200);
+        this.maskGraphics.fillRect(this.player.x, this.player.y, 64, 64);
     }
 }
